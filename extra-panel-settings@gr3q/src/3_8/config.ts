6b5845fef88889f03d65278fc2d10d0d5d149ab4
Gr3q <floyd0122@gmail.com>
2023-07-18 20:56:14 +0100

    Add new features, TODO: conditional enable

--- extra-panel-settings@gr3q/src/3_8/config.ts
+++ extra-panel-settings@gr3q/src/3_8/config.ts
@@ -3,8 +3,16 @@ import { Extension } from "./extension";
 const { ExtensionSettings } = imports.ui.settings;
 
 const CONFIG_KEYS = {
-    CUSTOM_FONT: "panelFont"
+    CUSTOM_FONT: "panelFont",
+    PANEL_COLOR: "panelColor",
+    PANEL_BORDER_RADIUS: "panelBorderRadius",
+    PANEL_PADDING: "panelPadding",
+    PANEL_MARGIN: "panelMargin",
 }
+
+const customKeys: (keyof typeof CONFIG_KEYS)[] = [
+    "CUSTOM_FONT"
+];
 
 export class Config {
     private readonly _panelFont!: string;
@@ -19,6 +27,32 @@ export class Config {
         return this.panelFontSize;
     }
 
+    private _panelColor!: string;
+    public get PanelColor(): string {
+        return this._panelColor;
+
+    }
+
+    private _panelBackgroundColor!: string;
+    public get PanelBackgroundColor(): string {
+        return this._panelBackgroundColor;
+    }
+
+    private _panelBorderRadius!: number;
+    public get PanelBorderRadius(): number {
+        return this._panelBorderRadius;
+    }
+
+    private _panelPadding!: number;
+    public get PanelPadding(): number {
+        return this._panelPadding;
+    }
+
+    private _panelMargin!: number;
+    public get PanelMargin(): number {
+        return this._panelMargin;
+    }
+
     settings = new ExtensionSettings(this, 'extra-panel-settings@gr3q');
     private readonly app: Extension; 
 
@@ -29,8 +63,18 @@ export class Config {
     Enable() {
         this.settings.bind(CONFIG_KEYS.CUSTOM_FONT, "_" + CONFIG_KEYS.CUSTOM_FONT, () => {
             this.ProcessSelectedFont();
-            this.app.UpdateCurrentFont();
+            this.app.ApplyStyles();
         });
+
+        let key: keyof typeof CONFIG_KEYS;
+        for (key in CONFIG_KEYS) {
+            if (customKeys.includes(key))
+                continue;
+
+            this.settings.bind(CONFIG_KEYS[key], "_" + CONFIG_KEYS[key], () => {
+                this.app.ApplyStyles();
+            });
+        }
 
         this.ProcessSelectedFont();
     }
